56af395ad6241b8e7b8dc6655dbba684477046ab
mikalaidudko <dudko.na@gmail.com>
2024-02-16 20:51:00 +0100

    [REFACTOR]

--- src/main/java/com/yosik/wenflon/Wenflon.java
+++ src/main/java/com/yosik/wenflon/Wenflon.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.lang.reflect.*;
-import java.util.Optional;
 
 public class Wenflon<T> {
 
@@ -15,18 +14,21 @@ public class Wenflon<T> {
     }
 
     public T createProxy() {
-        Class<?>[] interfaces1 =  implementations.stream()
+        final Class<?>[] commonInterfaces = getCommonInterfaces(); //todo come up with a dedicated exception
+        return (T) Proxy.newProxyInstance(
+                Wenflon.class.getClassLoader(),
+                commonInterfaces,
+                (proxy, method, args) -> implementations.get(0).getClass().getSimpleName()
+        );
+    }
+
+    private Class<?>[] getCommonInterfaces() {
+        return implementations.stream()
                 .map(impl -> Arrays.stream(impl.getClass().getInterfaces()).toList())
                 .reduce((a, b) -> {
                     var newA = new ArrayList<>(a);
                     newA.retainAll(b);
                     return newA;
-                }).orElseThrow().toArray(Class<?>[]::new); //todo come up with a dedicated exception
-        return (T) Proxy.newProxyInstance(
-                Wenflon.class.getClassLoader(),
-                interfaces1,
-                (proxy, method, args) ->
-                        implementations.get(0).getClass().getSimpleName()
-        );
+                }).orElseThrow().toArray(Class<?>[]::new);
     }
 }
