176e670746cf21a29645fd6af947b5830bc98aaf
mikalaidudko <dudko.na@gmail.com>
2024-02-16 20:11:35 +0100

    [GREEN] only interfaces present in all implementations are taken into account

--- src/main/java/com/yosik/wenflon/Wenflon.java
+++ src/main/java/com/yosik/wenflon/Wenflon.java
@@ -4,21 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.lang.reflect.*;
+import java.util.Optional;
 
-public class Wenflon <T>  {
+public class Wenflon<T> {
 
-    private final List<T> interfaces = new ArrayList<>(); //todo not sure if needed concurrent
+    private final List<T> implementations = new ArrayList<>(); //todo not sure if needed concurrent
 
-    public void add(T implementation){
-        interfaces.add(implementation);
+    public void add(T implementation) {
+        implementations.add(implementation);
     }
+
     public T createProxy() {
-        Class<?>[] interfaces1 = interfaces.get(0).getClass().getInterfaces();
+        Class<?>[] interfaces1 =  implementations.stream()
+                .map(impl -> Arrays.stream(impl.getClass().getInterfaces()).toList())
+                .reduce((a, b) -> {
+                    var newA = new ArrayList<>(a);
+                    newA.retainAll(b);
+                    return newA;
+                }).orElseThrow().toArray(Class<?>[]::new); //todo come up with a dedicated exception
         return (T) Proxy.newProxyInstance(
                 Wenflon.class.getClassLoader(),
                 interfaces1,
                 (proxy, method, args) ->
-                    interfaces.get(0).getClass().getSimpleName()
-                );
+                        implementations.get(0).getClass().getSimpleName()
+        );
     }
 }
--- src/test/java/com/yosik/wenflon/SimpleTest.java
+++ src/test/java/com/yosik/wenflon/SimpleTest.java
@@ -1,6 +1,8 @@
 package com.yosik.wenflon;
 
+import com.yosik.wenflon.test_classes.Notneeded;
 import com.yosik.wenflon.test_classes.ServiceA;
+import com.yosik.wenflon.test_classes.ServiceB;
 import com.yosik.wenflon.test_classes.Testable;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
@@ -12,5 +14,13 @@ public class SimpleTest {
         Wenflon<Testable> wenflon = new Wenflon<>();
         wenflon.add(new ServiceA()); //tmp
         assertThat( wenflon.createProxy().test()).isEqualTo( "ServiceA");
+        assertThat( wenflon.createProxy()).isInstanceOf(Testable.class);
+    }    @Test
+    void simpleTest2() {
+        Wenflon<Testable> wenflon = new Wenflon<>();
+        wenflon.add(new ServiceB()); //tmp
+        wenflon.add(new ServiceA()); //tmp
+        assertThat(wenflon.createProxy()).isInstanceOf(Testable.class);
+        assertThat(wenflon.createProxy()).isNotInstanceOf(Notneeded.class);
     }
 }
