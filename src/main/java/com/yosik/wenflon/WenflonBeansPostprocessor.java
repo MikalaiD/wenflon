91d41ccf6baf200b9f49967e600f839eecc8c38d
mikalaidudko <dudko.na@gmail.com>
2024-03-24 18:12:46 +0100

    [GREEN] regression passes, ready to implement conditions vs pivot

--- src/main/java/com/yosik/wenflon/WenflonBeansPostprocessor.java
+++ src/main/java/com/yosik/wenflon/WenflonBeansPostprocessor.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class WenflonBeansPostprocessor implements BeanPostProcessor {
 
-        private final WenflonRegistry wenflonRegistry;
+        private final WenflonRegistry wenflonRegistry = new WenflonRegistry(); //todo not sure if temp or permanent solution
         @Override
         public Object postProcessBeforeInitialization(final Object bean, final String beanName) throws BeansException {
             return bean;
@@ -21,10 +21,8 @@ public class WenflonBeansPostprocessor implements BeanPostProcessor {
             //here we assume class will implement only one interface under wenflon
             Optional<Class<?>> interfaceWithWenflon = Arrays.stream(bean.getClass().getInterfaces()).filter(aClass -> aClass.isAnnotationPresent(Wenflon.class)).findFirst();
             if (interfaceWithWenflon.isPresent()) {
-                //todo add a counter here, so in post postProcessAfterInitialization we know how many and what kind of bean is there
-                //can be async
                return wenflonRegistry.registerBehindWenflon(interfaceWithWenflon.get(), bean);
-            } //FINISHED here - make it return actual proxy
+            }
             return bean;
         }
 }
