d005c0b9c2dd35c73a3ad6c47ee2aa7953e05c96
mikalaidudko <dudko.na@gmail.com>
2024-03-24 18:02:37 +0100

    [RED] tests

--- src/test/java/com/yosik/wenflon/test_classes/ServiceA.java
+++ src/test/java/com/yosik/wenflon/test_classes/ServiceA.java
@@ -1,6 +1,8 @@
 package com.yosik.wenflon.test_classes;
 
-import com.yosik.wenflon.Wenflon;
+import com.yosik.wenflon.WenflonList;
 
+
+@WenflonList(conditions = "${non-wenflon-properties.not-cute}")
 public class ServiceA implements Testable {
 }
