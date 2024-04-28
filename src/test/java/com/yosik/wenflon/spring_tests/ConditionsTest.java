cc9c61a84fb2fb73b328246347e40c0396ad18ef
mikalaidudko <dudko.na@gmail.com>
2024-04-28 17:16:18 +0200

    [REFACTOR] refactored ConditionsTest

--- src/test/java/com/yosik/wenflon/spring_tests/ConditionsTest.java
+++ src/test/java/com/yosik/wenflon/spring_tests/ConditionsTest.java
@@ -1,22 +1,25 @@
 package com.yosik.wenflon.spring_tests;
 
 
-import com.yosik.wenflon.PivotProvider;
-import com.yosik.wenflon.WenflonBeanPostprocessor;
+import com.yosik.wenflon.WenflonProperties;
 import com.yosik.wenflon.spring_tests.test_classes.ServiceA;
-import com.yosik.wenflon.spring_tests.test_classes.ServiceB;
+import com.yosik.wenflon.spring_tests.test_classes.TestConfig;
 import com.yosik.wenflon.spring_tests.test_classes.Testable;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-public class ConditionsTest { //todo extract config as in bean creation test
+@EnableConfigurationProperties(WenflonProperties.class)
+@ContextConfiguration(classes = TestConfig.class)
+@TestPropertySource("classpath:application-test.properties")
+public class ConditionsTest { 
 
     @Autowired
     Testable primaryTestable;
@@ -27,33 +30,7 @@ public class ConditionsTest { //todo extract config as in bean creation test
     @Autowired
     @Qualifier("testableB")
     Testable testableB;
-
-//    @Autowired
-//    InterfaceWithoutImplementation interfaceWithoutImplementation;
-
-    @TestConfiguration
-    static class TestConfig {
-
-        @Bean
-        WenflonBeanPostprocessor factoryPostprocessor() {
-            return new WenflonBeanPostprocessor();
-        }
-
-        @Bean
-        Testable testableA() {
-            return new ServiceA();
-        }
-        @Bean
-        Testable testableB() {
-            return new ServiceB();
-        }
-
-        @Bean
-        PivotProvider<String> defaultPivotProvider(){
-            return ()->"panda";
-        }
-    }
-
+    
     @Test
     void condition_work(){
         String result = primaryTestable.test();
