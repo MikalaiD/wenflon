2b4b93589be4bf220dd6346cbd1542ba61d5aa98
Nick <MikalaiD@users.noreply.github.com>
2024-07-27 14:45:13 +0200

    [GREEN] added autoconfiguration

--- core/src/main/java/com/yosik/wenflon/Config.java
+++ core/src/main/java/com/yosik/wenflon/Config.java
@@ -1,11 +1,16 @@
 package com.yosik.wenflon;
 
 import java.util.List;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@AutoConfiguration
+@EnableConfigurationProperties(WenflonProperties.class)
 public class Config {
   @Bean
   @Primary
