c0db7b8097b54f594ec6eb7c4b01933d77665645
Nick <MikalaiD@users.noreply.github.com>
2024-08-08 20:57:26 +0200

    [REFACTOR] wrote javadoc for PivotProvider

--- core/src/main/java/com/github/mikalaid/wenflon/PivotProvider.java
+++ core/src/main/java/com/github/mikalaid/wenflon/PivotProvider.java
@@ -1,5 +1,12 @@
 package com.github.mikalaid.wenflon;
 
+/**
+ * Interface to return a pivot - a runtime value which will be tested against conditions; based on the outcome one or another
+ * implementation of {@link Wenflon} annotated interface will be used to run called method.
+ * Implementations of PivotProvider must be declared as spring beans. Multiple implementations are possible - then, to bind particular
+ * {@link Wenflon} with particular PivotProvider use bean names of the latter.
+ * @param <T> currently only String is supported
+ */
 @FunctionalInterface
 public interface PivotProvider<T>{
   T getPivot();
