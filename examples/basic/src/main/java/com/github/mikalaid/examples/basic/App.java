aa4be260bc3001f49436bc0a56b0a9208a9899eb
Nick <MikalaiD@users.noreply.github.com>
2024-08-16 21:27:25 +0200

    cleaanup

--- examples/basic/src/main/java/com/github/mikalaid/examples/basic/App.java
+++ examples/basic/src/main/java/com/github/mikalaid/examples/basic/App.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:examples/multiple_providers/src/main/java/com/yosik/examples/multiple_providers/App.java
-package com.yosik.examples.multiple_providers;
-========
 package com.github.mikalaid.examples.basic;
->>>>>>>> develop:examples/basic/src/main/java/com/github/mikalaid/examples/basic/App.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- examples/basic/src/main/java/com/github/mikalaid/examples/basic/config/user/Market.java
+++ examples/basic/src/main/java/com/github/mikalaid/examples/basic/config/user/Market.java
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:examples/multiple_providers/src/main/java/com/yosik/examples/multiple_providers/config/user/Market.java
-package com.yosik.examples.multiple_providers.config.user;
-========
 package com.github.mikalaid.examples.basic.config.user;
->>>>>>>> develop:examples/basic/src/main/java/com/github/mikalaid/examples/basic/config/user/Market.java
 
 public enum Market {
     EU,
