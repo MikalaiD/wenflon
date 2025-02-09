b19cd52483989f0093446009a8d443112cbc8a75
Nick <MikalaiD@users.noreply.github.com>
2025-02-09 21:02:31 +0100

    [GREEN] complex conditions allOf works with happy path

--- core/src/test/java/io/github/mikalaid/wenflon/test/conditions/complex_conditions/ConditionsTest.java
+++ core/src/test/java/io/github/mikalaid/wenflon/test/conditions/complex_conditions/ConditionsTest.java
@@ -1,11 +1,6 @@
 package io.github.mikalaid.wenflon.test.conditions.complex_conditions;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-import java.util.stream.Stream;
-
 import io.github.mikalaid.wenflon.core.PivotProvider;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,44 +14,52 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(SpringExtension.class)
 @ComponentScan("io.github.mikalaid.wenflon.core")
 @ContextConfiguration(classes = {TestConfig.class})
 @TestPropertySource("classpath:conditions/application-test_complex_conditions.properties")
 class ConditionsTest {
 
-  @Autowired
-  Testable underTest;
+    @Autowired
+    Testable underTest;
 
-  @SpyBean
-  @Qualifier("providerBeta")
-  PivotProvider<Integer> pivotProviderBeta;
+    @SpyBean
+    @Qualifier("providerBeta")
+    PivotProvider<Integer> pivotProviderBeta;
 
-  @SpyBean
-  @Qualifier("providerAlpha")
-  PivotProvider<String> pivotProviderAlpha;
+    @SpyBean
+    @Qualifier("providerAlpha")
+    PivotProvider<String> pivotProviderAlpha;
 
-  @ParameterizedTest
-  @MethodSource("getTestConfigurations") //todo write documentation for this happy path
-  void conditions_work_with_multiple_providers(String pivotAlpha, Integer pivotBeta, Class<?> implementation){
-    //given
-    when(pivotProviderAlpha.getPivot()).thenReturn(pivotAlpha);
-    when(pivotProviderBeta.getPivot()).thenReturn(pivotBeta);
+    @ParameterizedTest
+    @MethodSource("getTestConfigurations")
+        //todo write documentation for this happy path
+    void conditions_work_with_multiple_providers(String pivotAlpha, Integer pivotBeta, Class<?> implementation) {
+        //given
+        when(pivotProviderAlpha.getPivot()).thenReturn(pivotAlpha);
+        when(pivotProviderBeta.getPivot()).thenReturn(pivotBeta);
 
-    //when
-    var output = underTest.test();
+        //when
+        var output = underTest.test();
 
-    //then
-    assertThat(output).isEqualTo(implementation.getCanonicalName());
-  }
+        //then
+        assertThat(output).isEqualTo(implementation.getCanonicalName());
+    }
 
-  public static Stream<Arguments> getTestConfigurations() {
-    return Stream.of(
-            Arguments.of("panda", 1, ServiceA.class),
-            Arguments.of("duck", 404, ServiceB.class));
-  }
+    public static Stream<Arguments> getTestConfigurations() {
+        return Stream.of(
+                Arguments.of("panda", 1, ServiceA.class),
+                Arguments.of("panda", 42, ServiceA.class),
+                Arguments.of("ruanda", 1, ServiceA.class),
+                Arguments.of("duck", 404, ServiceB.class));
+    }
 
-  //todo think of the following thing: if user chooses complex conditions - shouldn't we enforce declaration of the default
-  // implementation? With complexity of conditions increase probability that condition will not match
-  // throwing exception in this case seems not a good idea.
+    //todo think of the following thing: if user chooses complex conditions - shouldn't we enforce declaration of the default
+    // implementation? With complexity of conditions increase probability that condition will not match
+    // throwing exception in this case seems not a good idea.
 }
