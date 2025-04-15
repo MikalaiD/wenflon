3351ce88e128891a3c20d02cd04f250c8acfa629
Nick <MikalaiD@users.noreply.github.com>
2025-04-15 19:13:37 +0200

    [GREEN] added todo

--- examples/complex_conditions/src/main/java/io/github/mikalaid/examples/complex_conditions/application/port/out/order/VetMedicineOrderProviderPort.java
+++ examples/complex_conditions/src/main/java/io/github/mikalaid/examples/complex_conditions/application/port/out/order/VetMedicineOrderProviderPort.java
@@ -5,6 +5,8 @@ import io.github.mikalaid.wenflon.core.Wenflon;
 import java.util.UUID;
 
 @Wenflon(pivotProviderBeanNames = {"chipInfoProvider", "petClassificatorProvider"})
+//todo to get rid of pivotProviderBeanNames - technically, if this is provided in the
+// properties then we can search by name and attach pivot provider to wenflon case
 //todo test if package-private will work after everything else is working
 public interface VetMedicineOrderProviderPort {
     boolean placeOrder(UUID medicineId, int count);
